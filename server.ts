cc630d9835568b6c8b5e80136c3b1cf011bb33ea
Chris T <john.doe@gmail.com>
2023-12-15 14:51:33 +0800

    build: new project

--- server.ts
+++ server.ts
@@ -1,1 +1,21 @@
-import express from 'express'
+import express from "express";
+import path from 'path'
+
+const app = express()
+
+app.get('/', async function (req: Request, res: Response) {
+    res.sendFile(path.resolve('public/html', 'home.html'))
+});
+
+app.use(express.static('public'))
+
+// handle 404 error
+app.use((req,res) => {
+    res.status(404);
+
+})
+
+const PORT = 8200;
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}/`)
+})
