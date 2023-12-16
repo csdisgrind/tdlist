686a3dae03e60484f0060cdc0e9c87850918dff1
Chris T <john.doe@gmail.com>
2023-12-17 00:44:06 +0800

    build: create list function but res cant be logged

--- public/js/todo.js
+++ public/js/todo.js
@@ -3,4 +3,42 @@ document.querySelector('#new-list').addEventListener('click', function() {
     newListDialog.show()
 })
 
+async function confirmCreateList(event){
+    console.log('submitted 1')
+    event.preventDefault()
+    console.log('passed default 1');
 
+    let form = event.target
+    // console.log({form})
+    let res = await fetch(form.action, {
+        method: form.method,
+        // ASKASK
+        // add-role.js 204 & student-profile 20 why no content-type?
+        headers: {
+            "Content-Type": form.enctype,
+            Accept: "application/json",
+          },
+        // when use body: new FormData(form), JSON.stringify
+        body: new URLSearchParams(new FormData(form)),
+    });
+    // fail to console log res
+
+    let result = await res.json()
+    console.log('hahihi', {result})
+
+    if (result.error) {
+        console.log(result.error);
+        alert(result.error)
+        return
+    }
+    console.log('successfully created a new list');
+}
+
+document.querySelector('#new-item').addEventListener('click', function(){
+    console.log('adding new item...')
+    newItemDialog.show()
+})
+
+function confirmCreateItem(event){
+    console.log('submitted 2');
+}
